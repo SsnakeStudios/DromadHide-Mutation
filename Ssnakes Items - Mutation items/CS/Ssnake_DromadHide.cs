54ade83386ee745f82a4b8b9cc0cce3d82fcd4f3
Ssnakes_Everywhere <74350577+SsnakeStudios@users.noreply.github.com>
2024-07-24 17:39:55 -0700

    Add files via upload

---
+++ Ssnakes Items - Mutation items/CS/Ssnake_DromadHide.cs
@@ -0,0 +1,51 @@
+using System;
+using XRL;
+using XRL.World;
+using XRL.World.Parts.Mutation;
+
+[Serializable]
+public class Ssnake_DromadHide : BaseMutation
+{
+	public Ssnake_DromadHide()
+	{
+		DisplayName = "Watery Reservoir";
+	}
+
+	public override bool WantEvent(int ID, int cascade)
+	{
+		if (!base.WantEvent(ID, cascade))
+		{
+			return ID == GetItemElementsEvent.ID;
+		}
+		return true;
+	}
+
+	public override bool HandleEvent(GetItemElementsEvent E)
+	{
+		if (E.IsRelevantCreature(ParentObject))
+		{
+			E.Add("salt", 1); //salt I believe is how much thirst is drained per turn, or related to it.
+		}
+		return base.HandleEvent(E);
+	}
+
+	public override void Register(GameObject Object, IEventRegistrar Registrar)
+	{
+		base.Register(Object, Registrar);
+	}
+
+	public override bool ChangeLevel(int NewLevel)
+	{
+		return base.ChangeLevel(NewLevel);
+	}
+
+	public override bool Mutate(GameObject GO, int Level)
+	{
+		return base.Mutate(GO, Level);
+	}
+
+	public override bool Unmutate(GameObject GO)
+	{
+		return base.Unmutate(GO);
+	}
+}
